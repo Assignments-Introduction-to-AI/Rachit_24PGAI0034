525df7d06555ccc21758d656812195505b584811
Rachit Shah <rshah240@gmail.com>
2023-08-12 17:00:53 +0530

    A star algorithm finished

--- Assignment_1/a_star.py
+++ Assignment_1/a_star.py
@@ -1,4 +1,5 @@
-# Functions to apply
+from queue import PriorityQueue
+### Functions to apply
 def get_connections(time_map: dict, node: str) -> list:
     """
     Function to get the connection list from the graph dictionary.\n
@@ -17,5 +18,55 @@ def get_connections(time_map: dict, node: str) -> list:
 
     return connections
 
+def astar(time_map: dict, dist_map: dict, start: str, end: str):
+    """
+    Function that implements the astar algorithm.\n
+    Args:\n
+        time_map: dict = Dictionary where the heuristic value is defined.\n
+        dist_map: dict = Dictionary where the path between two nodes is defined.\n
+        start: str = Start node.\n
+        end: str = End node.\n
+    Returns:\n
+        path
+    """
 
-# def astar(time_map)
+    # We are going to use Priority Queue to implement the a star algorithm
+    priority_queue = PriorityQueue()
+    priority_queue.put((0, start))
+    parent = {}
+    cost_so_far = {}
+    parent[start] = None
+    cost_so_far[start] = 0
+    found = False
+    
+    while not priority_queue.empty():
+        current = priority_queue.get()[1] # Using the index 1 to get the node name
+        if end == current:
+            found = True
+            break
+
+        connections = get_connections(time_map, current)
+        for i in connections:
+            new_cost = cost_so_far[current] + time_map[current][i]
+
+            if i not in cost_so_far or new_cost < cost_so_far[i]:
+                cost_so_far[i] = new_cost
+                priority_order = new_cost + dist_map[i][end]
+                priority_queue.put((priority_order,i))
+                parent[i] = current
+
+
+    if found:
+        path = []
+        node = end
+        while node != start:
+            path.append(node)
+            node = parent[node]
+
+        path.append(start)
+        path.reverse()
+
+        return path
+    
+    else:
+        return "Not Found"
--- Assignment_1/student_code.py
+++ Assignment_1/student_code.py
@@ -1,9 +1,11 @@
 from expand import expand
 from dfs import dfs_search
 from bfs import bfs_search
+from a_star import astar
 
 def a_star_search (dis_map, time_map, start, end):
-	pass
+	path = astar(time_map, dis_map, start, end)
+	return path
 
 def depth_first_search(time_map, start, end):
 	path = dfs_search(time_map, start, end)
